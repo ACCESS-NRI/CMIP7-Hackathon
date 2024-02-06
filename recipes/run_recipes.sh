fe2b96e1492ced1c15e2776b7c75c1d3a755ef5a
Romain Beucher <romain.beucher@anu.edu.au>
2024-02-07 09:58:15 +1100

    Split ocean recipes, add job.pbs template

---
+++ recipes/run_recipes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+maps=("recipes/maps/map1.yml" "recipes/maps/map2.yml" "recipes/maps/map3.yml" "recipes/maps/map4.yml" "recipes/maps/map5.yml")
+transects=("recipes/transects/transect1.yml" "recipes/transects/transect2.yml" "recipes/transects/transect3.yml")
+timeseries=("recipes/timeseries/timeseries1.yml" "recipes/timeseries/timeseries2.yml" "recipes/timeseries/timeseries3.yml" "recipes/timeseries/timeseries4.yml" "recipes/timeseries/timeseries5.yml" "recipes/timeseries/timeseries6.yml")
+depths=("recipes/depth_profiles/depth_integration.yml" "recipes/depth_profiles/depth_profile1.yml" "recipes/depth_profiles/depth_profile2.yml")
+
+models=( "${maps[@]}" "${transects[@]}" "${timeseries[@]}" "${depths[@]}" )
+
+
+for model in ${models[@]};
+do
+	qsub -v script=$model -N$(basename -- $model .yml) job.pbs
+done
+
+
+
+
