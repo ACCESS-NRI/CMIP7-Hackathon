f9cc2be42dc8f8cfa13efc614b61e624a33a60a3
Romain Beucher <romain.beucher@anu.edu.au>
2024-02-07 16:15:46 +1100

    Fix

--- run_recipes.sh
+++ run_recipes.sh
@@ -4,7 +4,7 @@
 
 atmos=("recipes/atmosphere/recipe_clouds_bias_precip.yml" "recipes/atmosphere/recipe_clouds_bias_surf_temp.yml" "recipes/atmosphere/recipe_clouds_bias_total_cloud_cover.yml" "recipes/atmosphere/recipe_clouds_ipcc.yml")
 land=("recipes/land/recipe_runoff_et.yml")
-seaice=("recipes/seaice/recipe_seaice.yml")
+seaice=("recipes/sea-ice/recipe_seaice.yml")
 general=("recipes/general/recipe_climwip_test_performance_sigma.yml"  "recipes/general/recipe_monitor.yml" "recipes/general/recipe_perfmetrics_CMIP56_simple_v3.yml")
 
 # Ocean recipes
