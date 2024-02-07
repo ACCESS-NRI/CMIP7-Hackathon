febb1d221ce94fa30ea7347eaa1a801940e28b89
Romain Beucher <romain.beucher@anu.edu.au>
2024-02-07 12:03:47 +1100

    Add utility function to run recipes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CMIP7-Hackathon
+
+
+How to run a recipe?
+
+
+source admin/bash_utilities
+run_recipe recipes/ocean/maps/map1.yml
--- run_recipes.sh
+++ run_recipes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ./admin/bash_utilities.sh
+
 maps=("recipes/maps/map1.yml" "recipes/maps/map2.yml" "recipes/maps/map3.yml" "recipes/maps/map4.yml" "recipes/maps/map5.yml" "recipes/maps/recipe_ocean_quadmap.yml")
 transects=("recipes/transects/transect1.yml" "recipes/transects/transect2.yml" "recipes/transects/transect3.yml")
 timeseries=("recipes/timeseries/timeseries1.yml" "recipes/timeseries/timeseries2.yml" "recipes/timeseries/timeseries3.yml" "recipes/timeseries/timeseries4.yml" "recipes/timeseries/timeseries5.yml" "recipes/timeseries/timeseries6.yml" "recipes/timeseries/timeseries_atlantic_stream_function.yml" "recipes/timeseries/timeseries_drake.yml")
@@ -8,9 +10,10 @@ depths=("recipes/depth_profiles/depth_integration.yml" "recipes/depth_profiles/d
 recipes=( "${maps[@]}" "${transects[@]}" "${timeseries[@]}" "${depths[@]}" )
 
 
+
 for recipe in ${recipess[@]};
 do
-	qsub -v recipe=$recipe -N$(basename -- $recipe .yml) -e admin/logs/$(basename -- $recipe .yml).stderr -o admin/logs/$(basename -- $recipe .yml).stdout  job.pbs
+  run_recipe $recipe
 done
 
 
