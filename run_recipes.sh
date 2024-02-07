fc4534095d5cb1bcf6ec4cac50467d8316a5c9cf
Romain Beucher <romain.beucher@anu.edu.au>
2024-02-07 14:12:06 +1100

    Fix run_recipes.sh

--- run_recipes.sh
+++ run_recipes.sh
@@ -2,14 +2,36 @@
 
 . ./admin/bash_utilities.sh
 
-maps=("recipes/maps/map1.yml" "recipes/maps/map2.yml" "recipes/maps/map3.yml" "recipes/maps/map4.yml" "recipes/maps/map5.yml" "recipes/maps/recipe_ocean_quadmap.yml")
-transects=("recipes/transects/transect1.yml" "recipes/transects/transect2.yml" "recipes/transects/transect3.yml")
-timeseries=("recipes/timeseries/timeseries1.yml" "recipes/timeseries/timeseries2.yml" "recipes/timeseries/timeseries3.yml" "recipes/timeseries/timeseries4.yml" "recipes/timeseries/timeseries5.yml" "recipes/timeseries/timeseries6.yml" "recipes/timeseries/timeseries_atlantic_stream_function.yml" "recipes/timeseries/timeseries_drake.yml")
-depths=("recipes/depth_profiles/depth_integration.yml" "recipes/depth_profiles/depth_profile1.yml" "recipes/depth_profiles/depth_profile2.yml")
+maps=("recipes/ocean/maps/map1.yml" "recipes/ocean/maps/map2.yml" "recipes/ocean/maps/map3.yml" "recipes/ocean/maps/map4.yml" "recipes/ocean/maps/map5.yml" "recipes/ocean/maps/recipe_ocean_quadmap.yml")
+transects=("recipes/ocean/transects/transect1.yml" "recipes/ocean/transects/transect2.yml" "recipes/ocean/transects/transect3.yml")
+timeseries=("recipes/ocean/timeseries/timeseries1.yml" "recipes/ocean/timeseries/timeseries2.yml" "recipes/ocean/timeseries/timeseries3.yml" "recipes/ocean/timeseries/timeseries4.yml" "recipes/ocean/timeseries/timeseries5.yml" "recipes/ocean/timeseries/timeseries6.yml" "recipes/ocean/timeseries/timeseries_atlantic_stream_function.yml" "recipes/ocean/timeseries/timeseries_drake.yml")
+depths=("recipes/ocean/depth_profiles/depth_integration.yml" "recipes/ocean/depth_profiles/depth_profile1.yml" "recipes/ocean/depth_profiles/depth_profile2.yml")
 
 recipes=( "${maps[@]}" "${transects[@]}" "${timeseries[@]}" "${depths[@]}" )
 
+if [[ $# != 0 ]]; then
 
+case $1 in
+
+	maps)
+		recipes=( "${maps[@]}" )
+	;;
+
+	transect)
+		recipes=("${transects[@]}")
+	;;
+
+	timseries)
+		recipes=("${transects[@]}")
+	;;
+
+	depths)
+		recipes=("${transects[@]}")
+	;;
+esac
+
+
+fi
 
 for recipe in ${recipes[@]};
 do
