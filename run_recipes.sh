8e3a1bbbcc77abe16f531e472ff6bbcdd5faf84d
Romain Beucher <romain.beucher@anu.edu.au>
2024-02-07 11:28:58 +1100

    change name from Model to Recipe for clarification

--- run_recipes.sh
+++ run_recipes.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-maps=("recipes/maps/map1.yml" "recipes/maps/map2.yml" "recipes/maps/map3.yml" "recipes/maps/map4.yml" "recipes/maps/map5.yml")
+maps=("recipes/maps/map1.yml" "recipes/maps/map2.yml" "recipes/maps/map3.yml" "recipes/maps/map4.yml" "recipes/maps/map5.yml" "recipes/maps/recipe_ocean_quadmap.yml")
 transects=("recipes/transects/transect1.yml" "recipes/transects/transect2.yml" "recipes/transects/transect3.yml")
-timeseries=("recipes/timeseries/timeseries1.yml" "recipes/timeseries/timeseries2.yml" "recipes/timeseries/timeseries3.yml" "recipes/timeseries/timeseries4.yml" "recipes/timeseries/timeseries5.yml" "recipes/timeseries/timeseries6.yml")
+timeseries=("recipes/timeseries/timeseries1.yml" "recipes/timeseries/timeseries2.yml" "recipes/timeseries/timeseries3.yml" "recipes/timeseries/timeseries4.yml" "recipes/timeseries/timeseries5.yml" "recipes/timeseries/timeseries6.yml" "recipes/timeseries/timeseries_atlantic_stream_function.yml" "recipes/timeseries/timeseries_drake.yml")
 depths=("recipes/depth_profiles/depth_integration.yml" "recipes/depth_profiles/depth_profile1.yml" "recipes/depth_profiles/depth_profile2.yml")
 
-models=( "${maps[@]}" "${transects[@]}" "${timeseries[@]}" "${depths[@]}" )
+recipes=( "${maps[@]}" "${transects[@]}" "${timeseries[@]}" "${depths[@]}" )
 
 
-for model in ${models[@]};
+for recipe in ${recipess[@]};
 do
-	qsub -v script=$model -N$(basename -- $model .yml) -e admin/logs/$(basename -- $model .yml).stderr -o admin/logs/$(basename -- $model .yml).stdout  job.pbs
+	qsub -v recipe=$recipe -N$(basename -- $recipe .yml) -e admin/logs/$(basename -- $recipe .yml).stderr -o admin/logs/$(basename -- $recipe .yml).stdout  job.pbs
 done
 
 
