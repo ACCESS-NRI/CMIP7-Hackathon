083d0e6534a7c0f1ca511109c6dd406bf8f70d5f
Romain Beucher <romain.beucher@anu.edu.au>
2024-02-09 13:57:56 +1100

    Use nf33

--- admin/bash_utilities.sh
+++ admin/bash_utilities.sh
@@ -4,17 +4,17 @@ pbs_job=$(cat <<-END
 #!/bin/bash -l 
 #PBS -S /bin/bash
 #PBS -P iq82
-#PBS -l storage=gdata/fs38+gdata/oi10+gdata/rr3+gdata/xp65+gdata/al33+gdata/rt52+gdata/zz93+scratch/tm70
+#PBS -l storage=gdata/fs38+gdata/oi10+gdata/rr3+gdata/xp65+gdata/al33+gdata/rt52+gdata/zz93+scratch/nf33
 #PBS -l wd
 #PBS -q normal
-#PBS -l walltime=00:30:00
+#PBS -l walltime=04:00:00
 #PBS -l mem=64GB
 #PBS -l ncpus=12
 
 module use /g/data/xp65/public/modules
 module load esmvaltool
 
-esmvaltool run --output_dir=/scratch/tm70/\$USER/esmvaltool_outputs \$recipe
+esmvaltool run --output_dir=/scratch/nf33/\$USER/esmvaltool_outputs \$recipe
 
 END
 
