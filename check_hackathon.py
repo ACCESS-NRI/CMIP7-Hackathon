ce9ed1d4485ad06c203d297fa7d3d52424b9ba98
Romain Beucher <romain.beucher@anu.edu.au>
2024-02-13 11:25:27 +1100

    Added some python utility functions

--- check_hackathon.py
+++ check_hackathon.py
@@ -22,6 +22,7 @@ pbs_job = """#!/bin/bash -l
 #PBS -l storage=gdata/fs38+gdata/oi10+gdata/rr3+gdata/xp65+gdata/al33+gdata/rt52+gdata/zz93+scratch/nf33+gdata/ct11
 #PBS -l wd
 #PBS -q copyq
+#PBS -W block=true
 #PBS -l walltime=04:00:00
 #PBS -l mem=64GB
 #PBS -l ncpus=1
@@ -32,14 +33,40 @@ module load esmvaltool
 esmvaltool run --output_dir=/scratch/nf33/\$USER/esmvaltool_outputs \$recipe
 """
 
+atmos=["recipes/atmosphere/recipe_clouds_bias_precip.yml", "recipes/atmosphere/recipe_clouds_bias_surf_temp.yml", "recipes/atmosphere/recipe_clouds_bias_total_cloud_cover.yml", "recipes/atmosphere/recipe_clouds_ipcc.yml"]
+land=["recipes/land/recipe_runoff_et.yml"]
+seaice=["recipes/sea-ice/recipe_seaice.yml"]
+general=["recipes/general/recipe_climwip_test_performance_sigma.yml", "recipes/general/recipe_monitor.yml", "recipes/general/recipe_perfmetrics_CMIP56_simple_v3.yml"]
+
+# Ocean recipes
+
+maps=["recipes/ocean/maps/map1.yml", "recipes/ocean/maps/map2.yml", "recipes/ocean/maps/map3.yml", "recipes/ocean/maps/map4.yml", "recipes/ocean/maps/map5.yml", "recipes/ocean/maps/recipe_ocean_quadmap.yml"]
+transects=["recipes/ocean/transects/transect1.yml", "recipes/ocean/transects/transect2.yml", "recipes/ocean/transects/transect3.yml"]
+timeseries=["recipes/ocean/timeseries/timeseries1.yml", "recipes/ocean/timeseries/timeseries2.yml", "recipes/ocean/timeseries/timeseries3.yml", "recipes/ocean/timeseries/timeseries4.yml", "recipes/ocean/timeseries/timeseries5.yml", "recipes/ocean/timeseries/timeseries6.yml", "recipes/ocean/timeseries/timeseries_atlantic_stream_function.yml", "recipes/ocean/timeseries/timeseries_drake.yml"]
+depths=["recipes/ocean/depth_profiles/depth_integration.yml", "recipes/ocean/depth_profiles/depth_profile1.yml", "recipes/ocean/depth_profiles/depth_profile2.yml"]
+
+
+def _get_pbs_run_command(recipe_path):
+        recipe_name=Path(recipe_path).stem
+        return f"qsub -v recipe={recipe_path} -N{recipe_name} -e admin/logs/{recipe_name}.stderr -o admin/logs/{recipe_name}.stdout <<< \"{pbs_job}\""
+
 def run_recipe(recipe_path):
 
     if os.path.isfile(recipe_path):
-        recipe_name=Path(recipe_path).stem
-        subprocess.run(f"qsub -v recipe={recipe_path} -N{recipe_name} -e admin/logs/{recipe_name}.stderr -o admin/logs/{recipe_name}.stdout <<< \"{pbs_job}\"", shell=True)
+        subprocess.run(_get_pbs_run_command(recipe_path), shell=True)
         print(f"Running recipe: {recipe_path}")
     else:
         print("{recipe_path} does not exist, please check the path.")
+
+def run_multiple_recipes(recipes_paths):
+
+    from subprocess import Popen
+    commands = [_get_pbs_run_command(recipe_path) for recipe_path in recipes_paths]
+    procs = [ Popen(i, shell=True) for i in commands ]
+    for p in procs:
+       p.wait()
+       rc = p.returncode
+       print(f"recipe return {rc} code")
 
 
 def user_belong_to_group(group):
@@ -99,3 +126,4 @@ if __name__ == '__main__':
     #check_read_access("/g/data/xp65/public/apps/esmvaltool")
     check_esmvaltool_config_file_exists()
     #run_recipe("./recipes/general/recipe_monitor.yml")
+    run_multiple_recipes(maps)
